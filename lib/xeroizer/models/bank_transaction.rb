8c17cfd14e0388783e79d45c813a30c64e1f5f65
JayChetty <jaypaulchetty@gmail.com>
2015-05-14 16:18:38 +0100

    only download full transaction for rate if not paginated

--- lib/xeroizer/models/bank_transaction.rb
+++ lib/xeroizer/models/bank_transaction.rb
@@ -65,7 +65,7 @@ module Xeroizer
       end
 
       def currency_rate
-        download_complete_record! if !complete_record_downloaded? || !paged_record_downloaded?
+        download_complete_record! if !paged_record_downloaded? && !complete_record_downloaded? 
         if attributes[:currency_rate]
           BigDecimal.new(attributes[:currency_rate])
         else
